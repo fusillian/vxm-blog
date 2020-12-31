a808a6338c24e8754153375721dd1c0c88a443a5
Andrei <fusillian@gmail.com>
2020-12-30 22:58:33 -0500

    Home.js: Click Event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Hello Ninjas", e);
+    };
+
+    const handleClickAgain = (name, e) => {
+        console.log(`hello ${name}`, e);
+    };
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button
+                onClick={(e) => {
+                    handleClickAgain("Mario", e);
+                }}
+            >
+                Click me again
+            </button>
         </div>
     );
 };
