4aa83db85dc3974b1b5a68617bf8bcbfe8a61487
Andrei <fusillian@gmail.com>
2020-12-30 19:29:44 -0500

    Adding Dynamic Variables to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 
 function App() {
+    const title = "Welcome to the new blog";
+    const likes = 50;
+const link = "http://10.0.0.22:3000"
+
     return (
         <div className="App">
             <div className="content">
-                <h1>App Components</h1>
+                <h1>{title}</h1>
+                <p>Liked {likes} times</p>
+
+                <p>{10}</p>
+                <p>{"Hello, ninja"}</p>
+                <p>{[1, 2, 3]}</p>
+                <a href={link}>Vidly Genres</a>
             </div>
         </div>
     );
