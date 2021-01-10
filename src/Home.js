e78db027bce734b26725417e7d460f5b9eab3a67
Andrei <fusillian@gmail.com>
2021-01-09 22:07:40 -0500

    Adding Age Variable to useState

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,26 @@
 import { useState } from "react";
 
+let initAge = 0;
+
 const Home = () => {
     // let name = "mew";
 
     const [name, setName] = useState("mew");
 
-    const [age, setAge] = useState(26);
+    const [age, setAge] = useState(initAge);
 
     const handleClick = () => {
         setName("mewtwo");
-        setAge(27);
+        setAge(age+1);
     };
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <p>
-                {name} is {age} years young
+                {name} is {age} years young 
+                <br></br>
+                Number of clicks: {age}
             </p>
             <button onClick={handleClick}>Click Me</button>
         </div>
