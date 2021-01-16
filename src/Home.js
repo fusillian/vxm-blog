a2be94488d900ad2b676b1557343c21dfbb029ed
Andrei <fusillian@gmail.com>
2021-01-16 17:35:10 -0500

    outputting lists final step

--- src/Home.js
+++ src/Home.js
@@ -22,11 +22,16 @@ const Home = () => {
         },
     ]);
 
-    return <div className="home">{blogs.map((blog) => 
-            <div className = "blog-preview" key={blog.id} >
-                
-            </div>
-        )}</div>;
+    return (
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title} </h2>
+                    <p>Written by {blog.author} </p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Home;
